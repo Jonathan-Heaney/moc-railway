ecbe6edacb77952e4f1db558b66fb25a66cf09b0
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-04 12:13:18 -0500

    Connect index.html to a view

--- contemporaries/views.py
+++ contemporaries/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import os
+from .models import FamousPerson
+import random
+import urllib.parse
 
 # Create your views here.
+
+
+# Function to generate Wikipedia links for famous people
+def generate_wikipedia_link(name):
+    formatted_name = urllib.parse.quote(name.replace(" ", "_"))
+    return f"https://en.wikipedia.org/wiki/{formatted_name}"
+
+
+def index(request):
+    return render(request, "contemporaries/index.html")
