85f4c88888969ada8f5f1f14e928df0ec8641d2b
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-27 09:47:51 -0500

    Collect static files

--- moc/settings.py
+++ moc/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["moc-render.onrender.com", "localhost",
+                 "127.0.0.1", "contemporari.es", "www.contemporari.es"]
 
 
 # Application definition
