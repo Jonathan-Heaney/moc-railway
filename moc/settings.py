28b69a8a9f7d2dea81196b9e479e13062b540a87
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-27 11:40:03 -0500

    Remove whitenoise for railway

--- moc/settings.py
+++ moc/settings.py
@@ -44,7 +44,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'contemporaries',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,7 +55,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
